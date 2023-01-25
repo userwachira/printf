93735406f2c4addd1255bf376fc6daf0413ef6b2
Wachira <catherinewachira6@gmail.com>
2023-01-25 11:48:04 -0800

    Update

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -27,7 +28,7 @@ struct fmt
 {
 	char fmt;
 	int (*fn)(va_list, char[], int, int, int, int);
-}
+};
 
 /**
  * typedef  struct fmt fmt_t - Struct op
@@ -109,4 +110,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-endif /*MAIN_H*/
+#endif /*MAIN_H*/
